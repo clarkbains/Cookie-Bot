db402285e302bbd6f259ca9a5bbd2eafc74beb72
Clark Bains <clark@clarkbains.com>
2019-11-10 01:06:00 -0500

    Created Basic Bot

---
+++ bot.js
@@ -0,0 +1,68 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const mysql = require ('mysql')
+const commands = require('./commands');
+
+function getOpts(filename){
+    const fs = require('fs');
+    let path = process.cwd()+"\\"+filename;
+    var buffer = fs.readFileSync(path);
+    let opts = {};
+    try{
+        opts = JSON.parse(buffer.toString());
+    }
+    catch (e){
+        console.error(e);
+        process.exit();
+    }
+    return opts;
+}
+function argsParse (msg){
+    let msgOpts = {}
+    msgOpts.senderName = msg.author.username;
+    msgOpts.senderId = msg.author.id;
+    msgOpts.content = msg.content.match(/;;cookies?\s*(.*$)/i)[1];
+    console.log(msgOpts)
+    msgOpts.channel = msg.channel.id;
+    msgOpts.client = msg.channel
+    if (msg.content.indexOf("bake")>-1){
+        msgOpts.type = "bake"
+    }
+    else if (msg.content.indexOf("list")>-1){
+        msgOpts.type = "list"
+    }
+    else {
+        msgOpts.type = "unknown"
+    }
+    return msgOpts
+}
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on('message', msg => {
+	if (!msg.author.bot){
+        //console.log(msg)
+        if (msg.content.match(/^;;cookie/i)) {
+            let msgOpts = argsParse(msg)
+            commands.run(client,con, msgOpts)
+        }
+    }
+});
+
+let opts = getOpts("auth.json");
+var con = mysql.createConnection({
+    host: opts.sqlHost,
+    user: opts.sqlUsername,
+    password: opts.sqlPassword,
+    database:"general"
+  });
+  
+  con.connect(function(err) {
+    
+    console.log("Connected to MySQL");
+  });
+
+client.login(opts.discordToken);
+setInterval(function(){commands.notify(client, con)}, 3000);
+
---
+++ commands.js
@@ -0,0 +1,51 @@
+module.exports = {
+    run: function (client,sqlclient,opts){
+        let served = true
+        if (opts.type == "bake"){
+            let cookieNum = opts.content.match(/bake\s*(\d+)/i)[1]
+            cookieNum = cookieNum || 1
+            let timeNow = Math.floor(new Date() / 1000)
+            console.log([opts.senderId, opts.channel,cookieNum,timeNow,timeNow,450])
+            sqlclient.query(`INSERT into cookieTracking (user, server, num, time_in, last_notify, temp) VALUES (?,?,?,?,?,450); `,
+            [opts.senderId, opts.channel,cookieNum,timeNow,timeNow,450],
+            (err,res)=>{
+                if (err){served=false; console.log(err)}
+                else{opts.client.send("I've put your cookies in the oven.")}
+
+            })
+        
+        }
+        else {
+            served = false
+        }
+        if (served==false){
+            opts.channel.send(`I didn't get that, sorry. Try \`;;cookie bake [cookies]\``)
+        }
+    },
+    notify: function (client,sqlclient){
+        console.log("Notifying:")
+        const timeToBake = 300;
+        const notifyEvery = 50;
+        let currentTime = Math.floor(new Date() / 1000)
+        let timeStart = currentTime-timeToBake
+        let timeNotify = currentTime-notifyEvery
+        sqlclient.query("SELECT * from cookieTracking WHERE (time_in>? and last_notify<?) or (baked=0 and time_in<=?)",[timeStart,timeNotify,timeStart],(err, res)=>{
+            for (let notification of res){
+                console.log(notification,currentTime - notification.time_in, currentTime-notification.last_notify)
+                if (currentTime - notification.time_in>timeToBake){
+                    //Finely aged cookie, update baked.
+                    client.channels.get(notification.server).send(`<@${notification.user}>, your cookie is done. Enjoy!`)
+                    sqlclient.query(`UPDATE cookieTracking SET baked=1 where id=${notification.ID}`)
+
+                }
+                else {
+                    let percentDone = Math.min(Math.round((currentTime-notification.time_in)/timeToBake * 1000) / 10,99);
+                    client.channels.get(notification.server).send(`<@${notification.user}>, your cookie is ${percentDone}% done. Stay tuned.`)
+            
+                }
+                sqlclient.query(`UPDATE cookieTracking SET last_notify = ? WHERE id=?`,[currentTime,notification.ID])
+        }
+        }
+        )
+    }
+};
